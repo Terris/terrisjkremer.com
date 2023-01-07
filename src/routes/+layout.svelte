30f350926d60bd1a21480783d2e063819f120ff0
Terris Kremer <terris.kremer@gmail.com>
2023-01-07 07:34:32 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	<header class="header">
+		<div class="wrapper">
+			<h1 class="logo"><a href="/">Terris James Kremer</a></h1>
+		</div>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer class="footer">
+		<div class="wrapper wrapper-lg">
+			<p class="copyright">&copy; 2023 Terris James Kremer</p>
+		</div>
+	</footer>
+</div>
+
+<style>
+	.header {
+		padding: 2rem 0;
+	}
+	.logo {
+		padding-top: 0;
+		font-size: 1rem;
+		font-weight: 700;
+	}
+	.logo a {
+		color: #fff;
+	}
+	.logo a:hover {
+		color: #f1c14d;
+	}
+	.footer {
+		font-size: 0.8rem;
+		color: #5c5c5c;
+		padding-bottom: 1rem;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,230 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="wrapper">
+	<h2 class="hero">
+		Hi, I'm Terris. I'm a <a href="https://github.com/Terris">front-end web developer</a> and
+		hobbyist writer, <a href="https://www.instagram.com/terrisjameskremer/">photographer</a>, and
+		musician.
+	</h2>
+	<nav class="nav-secondary">
+		<a href="https://github.com/Terris"><i class="fab fa-github" /></a>
+		<a href="https://www.linkedin.com/in/terris-kremer-828292147/"
+			><i class="fab fa-linkedin-in" /></a
+		>
+		<a href="https://www.instagram.com/terrisjameskremer/"><i class="fab fa-instagram" /></a>
+		<a href="https://dribbble.com/terrisj"><i class="fab fa-dribbble" /></a>
+	</nav>
+</div>
+
+<div class="wrapper wrapper-lg">
+	<hr />
+	<div class="projects">
+		<div class="projects-col">
+			<h3>Marketing Sites</h3>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://www.elevatedlawnutah.com/">Elevated Lawn</a>
+				</h4>
+				<p class="project-item-description">Marketing site for a Utah based lawn care company.</p>
+				<p class="project-item-tags">
+					Tags: Design, Responsive, HTML, CSS, JavaScript, ExpressionEngine CMS
+				</p>
+				<p class="project-item-links">
+					<a href="https://www.elevatedlawnutah.com/"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="http://laurencain.com/">Lauren Cain</a>
+				</h4>
+				<p class="project-item-description">
+					A personal portfolio and resume web site for digital strategist, Lauren Cain.
+				</p>
+				<p class="project-item-tags">
+					Tags: Design, Responsive, HTML, CSS, JavaScript, Static Templates
+				</p>
+				<p class="project-item-links">
+					<a href="https://www.elevatedlawnutah.com/"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="http://www.sidonievillere.com/">Sidonie Villere</a>
+				</h4>
+				<p class="project-item-description">
+					A personal portfolio web site for artist Sidonie Villere.
+				</p>
+				<p class="project-item-tags">
+					Tags: Design, Responsive, HTML, CSS, JavaScript, Static Templates
+				</p>
+				<p class="project-item-links">
+					<a href="http://www.sidonievillere.com/"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+		</div>
+
+		<div class="projects-col">
+			<h3>Software</h3>
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://dicey-three.vercel.app/">Dicey (work in progress)</a>
+				</h4>
+				<p class="project-item-description">A real-time dice game you can play with friends.</p>
+				<p class="project-item-tags">
+					Tags: Design, Next.js, React, Firebase, HTML, CSS, JavaScript,
+				</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/dicey"><i class="fab fa-github" /></a>
+					<a href="https://dicey-three.vercel.app/"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://github.com/Terris/redwood-stripe-example">BodaciousBots</a>
+				</h4>
+				<p class="project-item-description">
+					A web store and example RedwoodJS and Stripe integration.
+				</p>
+				<p class="project-item-tags">
+					Tags: RedwoodJS, Stripe, React, GraphQL, Apollo, HTML, CSS, JavaScript
+				</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/redwood-stripe-example"><i class="fab fa-github" /></a>
+					<a href="https://redwood-stripe-example.netlify.app/"
+						><i class="fas fa-external-link-alt" /></a
+					>
+				</p>
+			</div>
+		</div>
+
+		<div class="projects-col">
+			<h3>Micro Projects</h3>
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://redwoodjs.com/docs/how-to/gotrue-auth">RedwoodJS GoTrue Auth Recipe</a>
+				</h4>
+				<p class="project-item-description">
+					A walkthrough and example code for integrating GoTrueJS Auth with RedwoodJS
+				</p>
+				<p class="project-item-tags">Tags: Tutorial, RedwoodJS, GoTrueJS, JavaScript, React</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/redwood-gotrue-example"><i class="fab fa-github" /></a>
+					<a href="https://redwoodjs.com/docs/how-to/gotrue-auth"
+						><i class="fas fa-external-link-alt" /></a
+					>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://wordtohtml.terrisjkremer.com/">WordToHTML</a>
+				</h4>
+				<p class="project-item-description">Quickly convert Word docs to HTML.</p>
+				<p class="project-item-tags">Tags: SPA, HTML, CSS, JavaScript, React</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/wordToHTML"><i class="fab fa-github" /></a>
+					<a href="https://wordtohtml.terrisjkremer.com/"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://2690.terrisjkremer.com">2690</a>
+				</h4>
+				<p class="project-item-description">An experiment in Chess Board mechanics.</p>
+				<p class="project-item-tags">Tags: SPA, HTML, CSS, JavaScript, React, Redux</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/2690"><i class="fab fa-github" /></a>
+					<a href="https://2690.terrisjkremer.com"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+
+			<div class="project-item">
+				<h4 class="project-item-title">
+					<a href="https://maze.terrisjkremer.com">Matter Maze</a>
+				</h4>
+				<p class="project-item-description">Experimental game mechanics with Matter.js.</p>
+				<p class="project-item-tags">Tags: SPA, HTML, CSS, JavaScript, Matter.js</p>
+				<p class="project-item-links">
+					<a href="https://github.com/Terris/matter-maze"><i class="fab fa-github" /></a>
+					<a href="https://maze.terrisjkremer.com"><i class="fas fa-external-link-alt" /></a>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	/* Main
+  ----------------------------------------------- */
+	.nav-secondary a {
+		display: inline-block;
+		padding: 1rem 20px;
+		color: #fff;
+	}
+	.nav-secondary a:hover {
+		color: #ffe45c;
+	}
+
+	@media screen and (min-width: 820px) {
+		.hero {
+			width: 66.666666%;
+			padding-right: 40px;
+		}
+	}
+
+	/* Projects section
+  ----------------------------------------------- */
+
+	.projects .project-item .project-item-title {
+		padding-bottom: 0;
+		font-size: 1.15rem;
+		font-weight: 400;
+	}
+
+	.projects .project-item .project-item-description {
+		padding-top: 0;
+		padding-bottom: 0;
+	}
+	.projects .project-item .project-item-links {
+		padding-top: 0.5rem;
+		margin-left: -20px;
+	}
+	.projects .project-item .project-item-links a {
+		color: #fff;
+		margin-left: 20px;
+	}
+	.projects .project-item .project-item-links a:hover {
+		color: #ffe45c;
+	}
+	.projects .project-item .project-item-tags {
+		padding-top: 0;
+		padding-bottom: 0;
+		font-size: 0.8rem;
+		color: #5c5c5c;
+	}
+
+	@media screen and (min-width: 700px) {
+		.projects {
+			display: flex;
+			align-items: flex-start;
+			justify-content: space-between;
+		}
+		.projects .projects-col {
+			width: 33.333333%;
+			padding: 0 20px;
+		}
+		.projects .projects-col:first-child {
+			padding-left: 0;
+		}
+		.projects .projects-col:last-child {
+			padding-right: 0;
+		}
+	}
+</style>
